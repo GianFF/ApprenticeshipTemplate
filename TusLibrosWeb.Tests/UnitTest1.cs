967788da898bc6d603011b38183908c911df87de
marcos alvarenga <marcos.alvarenga@10pines.com>
2016-09-27 16:24:55 -0300

    Se agrega proyecto web..

---
+++ TusLibrosWeb.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TusLibrosWeb.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
