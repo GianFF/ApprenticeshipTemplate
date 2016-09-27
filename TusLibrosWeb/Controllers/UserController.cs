8d515d79f13f0076a562c29e105cac80b2aec926
marcos alvarenga <marcos.alvarenga@10pines.com>
2016-09-27 17:05:33 -0300

    Agregamos pantalla login y pudimos registrar un usuario

---
+++ TusLibrosWeb/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using TusLibros.app.environment;
+using TusLibros.app;
+using TusLibrosWeb.Models;
+
+namespace TusLibrosWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        private static DevelopmentEnvironment Environment = new DevelopmentEnvironment(new PersitentDataBaseStrategy());
+        private static IYourBooksApplication Application = Environment.GetApplication();
+
+        //Get login page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Post: login user
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model, string returnUrl)
+        {
+            Application.RegisterClient(model.UserName, model.Password);
+            return View();
+        }
+    }
+}
---
+++ TusLibrosWeb/Models/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TusLibros.app;
+using TusLibros.app.environment;
+
+namespace TusLibrosWeb.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        private static DevelopmentEnvironment Environment = new DevelopmentEnvironment(new PersitentDataBaseStrategy());
+        private static IYourBooksApplication Application = Environment.GetApplication();
+
+        public void Log()
+        {
+            Application.RegisterClient("qwe", "123");
+
+        }
+    }
+}
